9876c9f49a43b85bf562f2f8016ebcd296325124
BaileyCostello <BaileyCostello@users.noreply.github.com>
2025-10-23 03:14:41 +0000

    deploy: 6f0600518eafcecb2aa4e02727473e61f8bf9242

---
+++ _next/static/H4lEaYHwTg5VtKmUFDepN/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6efa7403302d35f4.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
